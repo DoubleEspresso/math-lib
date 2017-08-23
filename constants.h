a5efe9282381db7da34e27bf872a4e7313f392d8
Michael Glatzmaier <michael.glatzmaier@gmail.com>
2017-08-23 18:23:38 -0500

    refactors + add constants.h

---
+++ constants.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef BASE_CONSTANTS_H
+#define BASE_CONSTANTS_H
+
+#include <limits>
+
+#define PI 3.1415926535897932384626433832795028841971693993751
+#define TWO_PI 6.283185307179586476925286766559005768394
+
+#define MIN std::numeric_limits<double>::min()
+#define MAX std::numerica_limits<double>::max()
+
+#endif
